f4c1625d19ef1d8f6181c4299f94601a057d88c2
br34th3r <joshuaboddy1@gmail.com>
2021-10-23 21:40:22 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Here!");
+    loop {}
+}
